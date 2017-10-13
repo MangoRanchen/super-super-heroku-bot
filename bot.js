bcdd0c97f04e61889cfecea037aea55f37cb3046
MangoRanchen <32754089+MangoRanchen@users.noreply.github.com>
2017-10-12 21:07:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'Toxapex') {
+    	message.reply('who is this guy anyway lol');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
